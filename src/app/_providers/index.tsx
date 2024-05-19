56bbff803da5bc50121c915671ba05d5655b80ae
ErikPinel <erik.zusin@gmail.com>
2024-05-19 22:08:46 +0300

    first commit

--- src/app/_providers/index.tsx
+++ src/app/_providers/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable simple-import-sort/imports */
 'use client'
 
 import React from 'react'
@@ -5,6 +6,7 @@ import React from 'react'
 import { AuthProvider } from '../_providers/Auth'
 import { CartProvider } from '../_providers/Cart'
 import { ThemeProvider } from './Theme'
+import { FilterProvider } from './Filter'
 
 export const Providers: React.FC<{
   children: React.ReactNode
@@ -12,7 +14,9 @@ export const Providers: React.FC<{
   return (
     <ThemeProvider>
       <AuthProvider>
-        <CartProvider>{children}</CartProvider>
+        <FilterProvider>
+          <CartProvider>{children}</CartProvider>
+        </FilterProvider>
       </AuthProvider>
     </ThemeProvider>
   )
